0c5fc0339811be17ad405e25dc38c0486df3118e
MattHammond7 <hammondmatt666@gmail.com>
2016-05-16 20:13:04 +1200

    Working Pythagoras Calculator

---
+++ script.js
@@ -0,0 +1,30 @@
+/*
+Find out about how the JavaScript Math.sqrt() function works:
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sqrt
+
+Features to add:
+
+- calculate the result for the hypotenuse
+- display the hypotenuse result where it currently reads "result"
+- validate that the input values entered are greater than 0
+- display an error message for the user if the input values they are 0 or less
+
+*/
+
+document.getElementById('calculate').addEventListener('click', function() {
+  var width = document.getElementById('width').value;
+  var height = document.getElementById('height').value;
+  document.getElementById('triangle').setAttribute('width', width);
+  if (width<=0) {
+    alert("Negative width value")
+    document.getElementById('triangle').setAttribute('width', '0');
+  }
+  document.getElementById('triangle').setAttribute('height', height);
+  if (height<=0) {
+    alert("Negative height value")
+    document.getElementById('triangle').setAttribute('height', '0');
+  }
+  document.getElementById('hypotenuse').value = Math.sqrt(height*height+width*width);
+  
+});
+
